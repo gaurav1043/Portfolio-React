1e9f72f18b486387a6541eee356c669cd3b623fb
gaurav1043 <you@example.com>
2022-11-27 02:54:31 +0530

    first commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./portfolio.css";
+import img1 from "../../assets/portfolio1.jpg";
+import img2 from "../../assets/portfolio2.jpg";
+import img3 from "../../assets/portfolio3.jpg";
+import img4 from "../../assets/portfolio4.jpg";
+import img5 from "../../assets/portfolio5.png";
+import img6 from "../../assets/portfolio6.jpg";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={img1} alt="portfolio" />
+          </div>
+          <h3>This is Title</h3>
+          <a
+            href="#"
+            className="btn"
+            target="_blank
+            "
+          >
+            Github
+          </a>
+          <a href="#" className="btn btn-primary" target="_blank">
+            live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,103 @@
 import React from "react";
 import "./services.css";
+import { BsCheckLg } from "react-icons/bs";
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing eli .</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>
+                Quisquam id ratione odio dolores dignissimos assumenda fuga.
+              </p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Cum eos enim molestias quos, tenetur.</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consect elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>consectetur doloremque laborum tenetur.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Quisquam dignisssda fuga. Cum eos a molestias quos.</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Cum eos, consectetur doloreus odio pariatur tenetur.</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipilit.</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Quisquam id ratione obcaecati ssumenda fuga.</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Cum eos assumenda enim molestias tenetur.</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>consectetur doloremque alaborum tenetur.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. .</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Quisquam id ratione obcaecati odio quos.</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Cum eos assumenda enim molestias quos, tenetur.</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicin</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Quisquam id ratione obcgnissimos assumenda fuga.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
